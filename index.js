01f873f0bb548c87e58465d640c7dcefda1a5a95
arpitp1234 <92322250+arpitp1234@users.noreply.github.com>
2024-07-10 15:20:41 +0530

    first commit

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 ////-------------Database Connection---------------------------------/////
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost/refer_earn");
-
-const mysql=require("mysql")
-const db=mysql.createConnection({
-    host:"local",
-    user:'root',
-    password:'99Worst@hit',
-    database:'arpit',
-    table:'student'
-})
+/*mongoose.connect("mongodb+srv://redway345:nzRzvY4kxPKfEJxx@referearn.4ukocme.mongodb.net/?retryWrites=true&w=majority&appName=referearn")*/
 const hostname="127.0.0.1";
+//---------------------------------------------------------//
+const dotenv=require('dotenv');
+dotenv.config()
+mongoose.connect(process.env.URI)
 
 //-----------------------------------------------------------//
-
 const express=require('express');
 var cors=require('cors');
-
 const app=express();
 const port=5000;
 
 app.use(cors());
 app.use(express.json());
 
-
 //Available Routes
-
 app.use('/api/data',require('./routes/data'));
-
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
